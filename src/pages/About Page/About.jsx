e5aa1a533033976330e1168c170e381e7189ea65
Krisha Patel <kpate422@uic.edu>
2025-09-23 00:25:07 -0500

    Added initial about page with navy rectangles and green border with bolded titles

--- src/pages/About Page/About.jsx
+++ src/pages/About Page/About.jsx
@@ -1,1 +1,14 @@
-export default function About(){ return <h1>About</h1> }
+import React from "react";
+import "./About.css"; 
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="rectangle">History</div>
+      <div className="rectangle">Mission</div>
+      <div className="rectangle">Value</div>
+    </div>
+  );
+};
+
+export default About;
