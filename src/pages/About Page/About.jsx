2efe556d15a82c3873b26ed506499b97507dd6e0
Krisha Patel <kpate422@uic.edu>
2025-09-23 19:46:19 -0500

    added photos inside the history, mission, and value rectangles and created a photos folder

--- src/pages/About Page/About.jsx
+++ src/pages/About Page/About.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./About.css"; 
 
+// import images from my pictures folder
+import historyImg from "./pictures/history.png";
+import missionImg from "./pictures/mission.png";
+import valueImg from "./pictures/value.png";
+
 const About = () => {
   return (
     <div className="about-container">
-      <div className="rectangle">History</div>
-      <div className="rectangle">Mission</div>
-      <div className="rectangle">Value</div>
+      <div className="rectangle">
+        <img src={historyImg} alt="History" className="icon" />
+        <span>History</span>
+      </div>
+      <div className="rectangle">
+        <img src={missionImg} alt="Mission" className="icon" />
+        <span>Mission</span>
+      </div>
+      <div className="rectangle">
+        <img src={valueImg} alt="Value" className="icon" />
+        <span>Value</span>
+      </div>
     </div>
   );
 };
